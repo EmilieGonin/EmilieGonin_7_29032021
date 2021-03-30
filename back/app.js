871dd7e2970dd8cd2d23501e53442fbfc7e9e320
EmilieGonin <emilie.yuu@gmail.com>
2021-03-30 13:14:17 +0200

    Remove sequelize from app.js

--- back/app.js
+++ back/app.js
@@ -1,5 +1,3 @@
-const mysql = require("mysql2");
-const { Sequelize, DataTypes } = require("sequelize");
 const express = require('express');
 const bodyParser = require("body-parser");
 //const path = require("path");
@@ -8,16 +6,6 @@ const app = express();
 
 //Import routes
 const authRoutes = require("./routes/auth");
-
-//Create db if it doesn't already exist
-const connection = mysql.createConnection({ user: "root" });
-connection.query("CREATE DATABASE IF NOT EXISTS test");
-
-//Connect to db
-const sequelize = new Sequelize("test", "root", null, { dialect: "mysql" });
-sequelize.authenticate()
-.then(() => console.log('Connection has been established successfully.'))
-.catch((error) => console.error('Unable to connect to the database:', error));
 
 app.use((req, res, next) => {
 	res.setHeader("Access-Control-Allow-Origin", "*");
