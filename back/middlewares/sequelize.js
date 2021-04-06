bbc72c4c371bffe741e8957d04c40391cd45c1b3
EmilieGonin <emilie.yuu@gmail.com>
2021-04-06 16:20:40 +0200

    Remove sequelize from db object

--- back/middlewares/sequelize.js
+++ back/middlewares/sequelize.js
@@ -10,15 +10,15 @@ async function initialize() {
   await connection.query("CREATE DATABASE IF NOT EXISTS test");
 
   //Connect to db
-  db.sequelize = new Sequelize("test", "root", null, {
+  sequelize = new Sequelize("test", "root", null, {
     dialect: "mysql",
     logging: false
   });
 
   //Add models
-  db.User = require("../models/User")(db.sequelize, Sequelize);
-  db.Post = require("../models/Post")(db.sequelize, Sequelize);
-  db.Comment = require("../models/Comment")(db.sequelize, Sequelize);
+  db.User = require("../models/User")(sequelize, Sequelize);
+  db.Post = require("../models/Post")(sequelize, Sequelize);
+  db.Comment = require("../models/Comment")(sequelize, Sequelize);
 
   db.User.hasMany(db.Comment, { foreignKey: { allowNull: false } });
   db.Comment.belongsTo(db.User);
@@ -29,11 +29,11 @@ async function initialize() {
   db.Post.hasMany(db.Comment, { foreignKey: { allowNull: false } });
   db.Comment.belongsTo(db.Post);
 
-  await db.sequelize.authenticate()
+  await sequelize.authenticate()
   .then(() => console.log("Connexion à la base de données MySQL terminée !"))
   .catch((error) => console.error("Impossible de se connecter à la base de données :", error));
 
-  await db.sequelize.sync({ force: true })
+  await sequelize.sync({ force: true })
   .then(() => console.log("Base de données synchronisée avec succès !"))
   .catch((error) => console.error(error));
 }
