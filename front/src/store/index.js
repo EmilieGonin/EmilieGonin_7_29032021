d8b6ad5c553d2993bbe18826180fca651817f22b
EmilieGonin <emilie.yuu@gmail.com>
2021-04-12 11:58:03 +0200

    Add axios + add posts to vuex store

--- front/src/store/index.js
+++ front/src/store/index.js
@@ -1,11 +1,22 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
 
 export default createStore({
   state: {
+    posts: {}
   },
   mutations: {
+    SET_POSTS(state, posts) {
+      state.posts = posts
+    }
   },
   actions: {
+    getPosts({ commit }) {
+      axios.get("http://localhost:3000/api/posts")
+      .then(response => {
+        commit("SET_POSTS", response.data.posts)
+      })
+    }
   },
   modules: {
   }
