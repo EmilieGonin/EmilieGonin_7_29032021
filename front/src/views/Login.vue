e338632da481c76825eddaa67f28c151918649be
EmilieGonin <emilie.yuu@gmail.com>
2021-04-21 12:25:14 +0200

    Add login form actions

--- front/src/views/Login.vue
+++ front/src/views/Login.vue
@@ -1,10 +1,34 @@
 <template>
   <div class="login">
-    <form class="form" action="index.html" method="post">
-      //
+    <form class="form" @submit="loginForm" action="index.html" method="post">
+      <input type="email" name="email" id="email" v-model="email" />
+      <input type="password" name="password" id="password" v-model="password" />
+      <button type="submit">Se connecter</button>
     </form>
+    <router-link to="/signup">
+      S'inscrire
+    </router-link>
   </div>
 </template>
+
+<script>
+export default {
+  name: "Login",
+  methods: {
+    loginForm(e) {
+      e.preventDefault();
+      const user = {
+        email: this.email,
+        password: this.password
+      };
+      this.$store
+        .dispatch("login", user)
+        .then(() => this.$router.push("/"))
+        .catch(() => console.error("error"));
+    }
+  }
+};
+</script>
 
 <style lang="scss">
 // Importing the global css file
