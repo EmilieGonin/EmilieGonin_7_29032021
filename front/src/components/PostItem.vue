79658b70c23c35b4f7dad1bc84ea024b5c7d6a2d
EmilieGonin <emilie.yuu@gmail.com>
2021-04-26 14:01:34 +0200

    Add user avatar on post and fix displayed post name

--- front/src/components/PostItem.vue
+++ front/src/components/PostItem.vue
@@ -1,9 +1,15 @@
 <template>
   <div class="post-item">
     <div class="post-item__header">
-      <img class="post-item__avatar" src="@/assets/default.jpg" alt="" />
+      <img
+        class="post-item__avatar"
+        v-bind:src="User.avatar"
+        alt=""
+        v-if="User.avatar != null"
+      />
+      <img class="post-item__avatar" src="@/assets/default.jpg" alt="" v-else />
       <router-link class="post-item__name" :to="'/user/' + user.id">
-        {{ user.firstName }} {{ user.lastName }}
+        {{ User.firstName }} {{ User.lastName }}
       </router-link>
     </div>
     <div class="post-item__message">
@@ -22,7 +28,8 @@ export default {
   name: "PostItem",
   props: {
     id: Number,
-    text: String
+    text: String,
+    User: Object
   },
   computed: mapGetters(["user"])
 };
