ee65b57bd17254c4c18bdeedd895aa539c855750
EmilieGonin <emilie.yuu@gmail.com>
2021-04-26 14:49:53 +0200

    Load multer before auth middleware on user update route

--- back/routes/user.js
+++ back/routes/user.js
@@ -7,7 +7,7 @@ const controller = require("../controllers/user");
 router.get("/:id", auth, controller.getUser);
 router.post("/signup", controller.userSignup);
 router.post("/login", controller.userLogin);
-router.put("/:id", auth, multer, controller.userUpdate);
+router.put("/:id", multer, auth, controller.userUpdate);
 router.delete("/:id", auth, controller.userDelete);
 
 module.exports = router;
