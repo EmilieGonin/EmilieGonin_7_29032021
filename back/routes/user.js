bea813f63852c4c5075aef2a4d36b9ae7867a175
EmilieGonin <emilie.yuu@gmail.com>
2021-04-26 12:27:34 +0200

    Add auth middleware

--- back/routes/user.js
+++ back/routes/user.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const auth = require("../middlewares/auth");
 const multer = require("../middlewares/multer");
 const controller = require("../controllers/user");
 
-router.get("/:id", controller.getUser);
+router.get("/:id", auth, controller.getUser);
 router.post("/signup", controller.userSignup);
 router.post("/login", controller.userLogin);
-router.put("/:id", multer, controller.userUpdate);
-router.delete("/:id", controller.userDelete);
+router.put("/:id", auth, multer, controller.userUpdate);
+router.delete("/:id", auth, controller.userDelete);
 
 module.exports = router;
