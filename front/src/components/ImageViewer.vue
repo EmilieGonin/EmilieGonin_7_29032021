26d5b40790107ca3b07cd7bc5f6276de2110e1f5
EmilieGonin <emilie.yuu@gmail.com>
2021-05-14 11:05:35 +0200

    Improve ImageViewer size for mobile and resize

--- front/src/components/ImageViewer.vue
+++ front/src/components/ImageViewer.vue
@@ -2,14 +2,14 @@
   <div class="image-viewer" v-if="image">
     <div
       class="image-viewer__container"
-      :style="setHeight"
+      :style="setSize"
       @click="$emit('closeViewer')"
     ></div>
-    <div class="image-viewer__content" :style="setHeight">
+    <div class="image-viewer__content" :style="setSize">
       <div class="image-viewer__button" @click="$emit('closeViewer')">
         <i class="fas fa-times fa-fw"></i>
       </div>
-      <img :src="image" :style="setHeight" alt="" />
+      <img :src="image" :style="setSize" alt="" />
     </div>
   </div>
 </template>
@@ -19,13 +19,30 @@ export default {
   name: "ImageViewer",
   data() {
     return {
-      setHeight: {
-        maxHeight: window.innerHeight + "px"
-      }
+      windowHeight: "",
+      windowWidth: ""
     };
+  },
+  mounted() {
+    this.checkSize();
+    window.addEventListener("resize", this.checkSize);
   },
   props: {
     image: String
+  },
+  computed: {
+    setSize() {
+      return {
+        maxHeight: this.windowHeight + "px",
+        maxWidth: this.windowWidth + "px"
+      };
+    }
+  },
+  methods: {
+    checkSize() {
+      this.windowHeight = window.innerHeight;
+      this.windowWidth = window.innerWidth;
+    }
   }
 };
 </script>
@@ -36,7 +53,7 @@ export default {
 
 .image-viewer {
   &__container {
-    position: absolute;
+    position: fixed;
     top: 0;
     left: 0;
     z-index: 98;
@@ -46,7 +63,7 @@ export default {
     text-align: center;
   }
   &__content {
-    position: absolute;
+    position: fixed;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
