a8ed36d0fbfa4c449798da5d6596cff5735bbbb5
EmilieGonin <emilie.yuu@gmail.com>
2021-05-19 12:11:00 +0200

    Add year and month intervals

--- front/src/components/DateItem.vue
+++ front/src/components/DateItem.vue
@@ -22,26 +22,52 @@ export default {
       const date = new Date(this.date);
       const today = new Date(Date.now());
 
+      const yearInterval = date.getYear() - today.getYear();
+      const monthInterval = date.getMonth() - today.getMonth();
       const dayInterval = date.getDate() - today.getDate();
       const minutesInterval = date.getMinutes() - today.getMinutes();
       const secondsInterval = date.getSeconds() - today.getSeconds();
 
-      if (dayInterval == 0 && minutesInterval != 0) {
-        return new Intl.RelativeTimeFormat("fr").format(
-          minutesInterval,
-          "minutes"
-        );
-      } else if (dayInterval == 0 && minutesInterval == 0) {
-        if (secondsInterval == 0) {
-          return "à l'instant";
-        } else {
-          return new Intl.RelativeTimeFormat("fr").format(
-            secondsInterval,
-            "seconds"
-          );
-        }
+      //Year
+      if (yearInterval == -1) {
+        return "l'année dernière";
+      } else if (yearInterval == 1) {
+        return new Intl.RelativeTimeFormat("fr").format(yearInterval, "year");
       } else {
-        return new Intl.RelativeTimeFormat("fr").format(dayInterval, "day");
+        //Month
+        if (monthInterval == -1) {
+          return "le mois dernier";
+        } else if (monthInterval != 0) {
+          return new Intl.RelativeTimeFormat("fr").format(
+            monthInterval,
+            "month"
+          );
+        } else {
+          //Day
+          if (dayInterval == -1) {
+            return "hier";
+          } else if (dayInterval != 0) {
+            return new Intl.RelativeTimeFormat("fr").format(dayInterval, "day");
+          } else {
+            //Minutes
+            if (minutesInterval != 0) {
+              return new Intl.RelativeTimeFormat("fr").format(
+                minutesInterval,
+                "minutes"
+              );
+            } else {
+              //Seconds
+              if (secondsInterval == 0) {
+                return "à l'instant";
+              } else {
+                return new Intl.RelativeTimeFormat("fr").format(
+                  secondsInterval,
+                  "seconds"
+                );
+              }
+            }
+          }
+        }
       }
     }
   }
