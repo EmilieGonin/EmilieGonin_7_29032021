59ff9d6dd8aad57d46517ab8377c75efef81858e
EmilieGonin <emilie.yuu@gmail.com>
2021-05-19 12:45:52 +0200

    Install date-fns to improve posts & comments date and time intervals

--- front/src/components/DateItem.vue
+++ front/src/components/DateItem.vue
@@ -3,6 +3,9 @@
 </template>
 
 <script>
+import { format, formatDistanceToNow } from "date-fns";
+import fr from "date-fns/locale/fr";
+
 export default {
   name: "DateItem",
   props: {
@@ -10,74 +13,16 @@ export default {
   },
   computed: {
     getDate() {
-      const date = new Date(this.date);
-      return date.toLocaleDateString("fr", {
-        month: "long",
-        day: "numeric",
-        hour: "numeric",
-        minute: "numeric"
+      return format(new Date(this.date), "dd MMMM yyyy", {
+        locale: fr
       });
     },
     getTimeAgo() {
-      const date = new Date(this.date);
-      const today = new Date(Date.now());
-
-      const yearInterval = date.getYear() - today.getYear();
-      const monthInterval = date.getMonth() - today.getMonth();
-      const dayInterval = date.getDate() - today.getDate();
-      const hourInterval = date.getHours() - today.getHours();
-      const minutesInterval = date.getMinutes() - today.getMinutes();
-      const secondsInterval = date.getSeconds() - today.getSeconds();
-
-      //Year
-      if (yearInterval == -1) {
-        return "l'année dernière";
-      } else if (yearInterval == 1) {
-        return new Intl.RelativeTimeFormat("fr").format(yearInterval, "year");
-      } else {
-        //Month
-        if (monthInterval == -1) {
-          return "le mois dernier";
-        } else if (monthInterval != 0) {
-          return new Intl.RelativeTimeFormat("fr").format(
-            monthInterval,
-            "month"
-          );
-        } else {
-          //Day
-          if (dayInterval == -1) {
-            return "hier";
-          } else if (dayInterval != 0) {
-            return new Intl.RelativeTimeFormat("fr").format(dayInterval, "day");
-          } else {
-            //Hours
-            if (hourInterval != 0) {
-              return new Intl.RelativeTimeFormat("fr").format(
-                hourInterval,
-                "hours"
-              );
-            } else {
-              //Minutes
-              if (minutesInterval != 0) {
-                return new Intl.RelativeTimeFormat("fr").format(
-                  minutesInterval,
-                  "minutes"
-                );
-              } else {
-                //Seconds
-                if (secondsInterval == 0) {
-                  return "à l'instant";
-                } else {
-                  return new Intl.RelativeTimeFormat("fr").format(
-                    secondsInterval,
-                    "seconds"
-                  );
-                }
-              }
-            }
-          }
-        }
-      }
+      return formatDistanceToNow(new Date(this.date), {
+        addSuffix: true,
+        includeSeconds: true,
+        locale: fr
+      });
     }
   }
 };
